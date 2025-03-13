6d4c2611f39076dbcbca3826c6673de08f019e68
judgoly <judgoly@outlook.com>
2025-03-13 08:07:04 +0800

    完善配置文件，在优化界面

--- root/usr/lib/lua/luci/model/cbi/network-manager.lua
+++ root/usr/lib/lua/luci/model/cbi/network-manager.lua
@@ -1,7 +1,11 @@
-m = Map("network-manager", translate("网络接口守护设置"))
+m = Map("network-manager", translate("网络接口守护设置"), 
+    translate("用于管理网络接口的定时重启和监控"))
 
-s = m:section(TypedSection, "basic", translate("基本设置"))
-s.addremove = false
+-- 基本设置
+s = m:section(TypedSection, "basic", translate("基本设置"), 
+    translate("基础网络守护配置"))
+s.addremove = false  -- 禁止动态添加/删除配置节
+s.anonymous = true   -- 隐藏配置节名称显示
 
 o = s:option(Flag, "enabled", translate("启用服务"))
 o.default = 0
@@ -15,8 +19,11 @@ o = s:option(Value, "time", translate("每日重启时间"))
 o.datatype = "timehhmm"
 o.placeholder = "03:30"
 
-s = m:section(TypedSection, "advanced", translate("高级设置"))
+-- 高级设置
+s = m:section(TypedSection, "advanced", translate("高级设置"),
+    translate("高级网络检测参数"))
 s.addremove = false
+s.anonymous = true
 
 o = s:option(Value, "ping_host", translate("检测主机"))
 o.default = "8.8.8.8"
